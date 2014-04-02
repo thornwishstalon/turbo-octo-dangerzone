f3194db51873fd694fedb142799599ef1ae0b804
Fabian <fabian.pechstein@gmail.com>
2014-04-02 19:10:18 +0200

    Testing file draft

---
+++ src/main/input/command/commands/Testing.java
@@ -0,0 +1,27 @@
+package main.input.command.commands;
+
+import main.input.command.ICommand;
+
+public class Testing implements ICommand {
+
+	@Override
+	public int numberOfParams() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String execute(String[] params) {
+		// TODO Auto-generated method stub
+		
+		
+		/*
+		 * 
+		 * TODO 1. testing mit current index.. search for stuff
+		 * 		2. reader benutzen um topics einzulesen
+		 * 			-> docProcessor -> filtering -> alle 'relevanten' wörter aus der anfrage 
+		 */
+		return null;
+	}
+
+}
